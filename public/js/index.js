a0094e57581e0e7da738555da137edf63557bdc3
Juan Balceda <juanbalceda@gmail.com>
2018-06-18 23:58:50 +0200

    Firebase Google SignIn OK

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,35 @@
-console.log(firebase)
+var btnLogin = document.getElementById("btnLogin")
+var btnLogout = document.getElementById("btnLogout")
+
+firebase.auth().onAuthStateChanged(function(user){
+  if (user) {
+    console.log("Tenemos usuario")
+    btnLogin.style.display = "none";
+    btnLogout.style.display = "block";
+  } else {
+    console.log("No tenemos usuario")
+    btnLogout.style.display = "none";
+    btnLogin.style.display = "block";
+  }
+});
+
+btnLogin.addEventListener("click",function(){
+  event.preventDefault();
+  
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  
+  firebase.auth().signInWithPopup(provider)
+  .then(function(datosUsuario){
+    console.log(datosUsuario)
+  }).catch(function(err){
+    console.log(err)
+  })
+});
+
+btnLogout.addEventListener("click",function(){
+  firebase.auth().signOut()
+  .catch(function(err){
+    console.log(err)
+  });
+});
